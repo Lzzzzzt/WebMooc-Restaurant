0be4d8d1370b1b4116c670712ed40e59e548dadc
Lzzzzzt <liuzitao0123@gmail.com>
2022-06-26 10:19:24 +0800

    init

---
+++ src/components/RestaurantChef.vue
@@ -0,0 +1,18 @@
+<template>
+  <img src="src/assets/chef.png" alt="chef" class="chef">
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+.chef {
+  width: 100px;
+  height: 100px;
+  border: white solid 10px;
+  border-radius: 60px;
+
+  background-color: rgba(128, 128, 128, 0.5);
+}
+</style>
